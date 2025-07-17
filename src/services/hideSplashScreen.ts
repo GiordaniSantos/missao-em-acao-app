52b3ba960fcf4dd5621b95cc2920144d4378cbc3
Giordani <dani.santossilveira@hotmail.com>
2025-07-17 20:14:02 -0300

    ajuste hideSplashScreen

--- src/services/hideSplashScreen.ts
+++ src/services/hideSplashScreen.ts
@@ -1,6 +1,5 @@
 import * as SplashScreen from 'expo-splash-screen';
 
 export const hideSplashScreen = async () => {
-    await SplashScreen.preventAutoHideAsync();
     await SplashScreen.hideAsync();
 };
